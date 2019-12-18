3b8718d6a49b804dbbbad454b2ec5ee667644f7c
ihab <ihab@utopios.net>
2019-12-18 10:11:39 +0100

    cours heritage

---
+++ coursCSharp/coursCSharp/Classes/Etudiant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace coursCSharp.Classes
+{
+    public class Etudiant : Personne
+    {
+        private int niveau;
+
+        public Etudiant() : base()
+        {
+
+        }
+
+        public Etudiant(string n, string p) : base(n,p)
+        {
+
+        }
+
+        public Etudiant(string n, string p, int l) : base(n, p)
+        {
+            Niveau = l;
+        }
+
+        public int Niveau { get => niveau; set => niveau = value; }
+
+        public void EditPropsProtected(string p)
+        {
+            propsProtected = p;
+        }
+    }
+}
---
+++ coursCSharp/coursCSharp/Classes/Personne.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace coursCSharp.Classes
+{
+    public class Personne
+    {
+        private string nom;
+        private string prenom;
+        protected string propsProtected;
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+
+        public Personne()
+        {
+
+        }
+
+        public Personne(string n, string p)
+        {
+            Nom = n;
+            Prenom = p;
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine("Nom : " + Nom + " Prénom : " + Prenom);
+        }
+    }
+}
