2672eba50843d36efe2ed6ec0faa08b86e5d4ba6
ihab <ihab@utopios.net>
2020-01-06 10:00:54 +0100

    Cours syntaxe avancée csharp et operateur

--- CoursCSharp/coursCSharp/Classes/Point.cs
+++ CoursCSharp/coursCSharp/Classes/Point.cs
@@ -20,6 +20,6 @@ namespace coursCSharp.Classes
         public static Point operator +(Point p1, Point p2) => new Point { x = p1.X + p2.X, Y = p1.Y + p2.Y };
 
         public static bool operator ==(Point p1, Point p2) => p1.X==p2.X && p1.Y == p2.Y;
-        public static bool operator !=(Point p1, Point p2)!(p1.X == p2.X && p1.Y == p2.Y);
+        public static bool operator !=(Point p1, Point p2) => !(p1.X == p2.X && p1.Y == p2.Y);
     }
 }
