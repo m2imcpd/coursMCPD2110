79f5ff2cb893525408ec131dc823bba69d02190b
ihab <ihab@utopios.net>
2020-01-17 10:02:32 +0100

    cours tdd partie 3

--- CoursCSharp/CoursCSharpTest/JeuTest.cs
+++ CoursCSharp/CoursCSharpTest/JeuTest.cs
@@ -1,5 +1,6 @@
 ﻿using coursCSharp.Classes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -14,8 +15,12 @@ namespace CoursCSharpTest
         public void JouerTest()
         {
             Jeu jeu = new Jeu();
-            IDe de1 = new FauxDe(5);
-            IDe de2 = new FauxDe(3);
+            //IDe de1 = new FauxDe(5);
+            //IDe de2 = new FauxDe(3);
+            IDe de1 = Mock.Of<IDe>();
+            IDe de2 = Mock.Of<IDe>();
+            Mock.Get(de1).Setup(m => m.Valeur).Returns(5);
+            Mock.Get(de2).Setup(m => m.Valeur).Returns(3);
             bool result = jeu.Jouer(de1, de2);
             Assert.IsTrue(result);
         }
