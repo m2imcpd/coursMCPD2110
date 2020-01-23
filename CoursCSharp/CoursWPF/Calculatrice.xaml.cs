46a4b0d1ce450c0eef87a14cf331df1e775e884f
ihab <ihab@utopios.net>
2020-01-23 16:16:11 +0100

    Correction calculatrice wpf

--- CoursCSharp/CoursWPF/Calculatrice.xaml.cs
+++ CoursCSharp/CoursWPF/Calculatrice.xaml.cs
@@ -20,6 +20,12 @@ namespace CoursWPF
     public partial class Calculatrice : Window
     {
         private string[] tab = new string[] { "CE", "C", "supprimer", "%", "7", "8", "9", "X", "4", "5", "6", "-", "1", "2", "3", "+", "(+/-)", "0", ",", "=" };
+        private Label l;
+
+        
+        private bool NewNumber = true;
+        private string operation;
+        private double resultat;
         public Calculatrice()
         {
             InitializeComponent();
@@ -40,7 +46,8 @@ namespace CoursWPF
 
         private void CreateElements()
         {
-            Label l = new Label() { Content = "0", VerticalContentAlignment = VerticalAlignment.Center, HorizontalContentAlignment = HorizontalAlignment.Right };
+            l = new Label() { Content = "0", VerticalContentAlignment = VerticalAlignment.Center, HorizontalContentAlignment = HorizontalAlignment.Right };
+            l.FontSize = 40;
             maGrille.Children.Add(l);
             Grid.SetColumn(l, 0);
             Grid.SetRow(l, 0);
@@ -51,11 +58,107 @@ namespace CoursWPF
                 for(int j=0; j < 4; j++)
                 {
                     Button b = new Button { Content = tab[index] };
+                    b.Click += ClickElement;
                     index++;
                     maGrille.Children.Add(b);
                     Grid.SetColumn(b, j);
                     Grid.SetRow(b, i);
                 }
+            }
+        }
+
+        public void ClickElement(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            string valueBouton = b.Content.ToString();
+            int nombre;
+            if(Int32.TryParse(valueBouton, out nombre))
+            {
+                if (NewNumber)
+                {
+                    l.Content = valueBouton;
+                    NewNumber = false;
+                }
+                else
+                {
+                    l.Content += valueBouton;
+                }
+            }
+            else
+            {
+                NewNumber = true;
+                switch(valueBouton)
+                {
+                    case "+":
+                        if(operation != null)
+                        {
+                            Calcule(operation);
+                        }
+                        else
+                        {
+                            resultat = Convert.ToDouble(l.Content);
+                        }
+                        operation = "+";
+                        break;
+                    case "-":
+                        if (operation != null)
+                        {
+                            Calcule(operation);
+                        }
+                        else
+                        {
+                            resultat = Convert.ToDouble(l.Content);
+                        }
+                        operation = "-";
+                        break;
+                    case "X":
+                        if (operation != null)
+                        {
+                            Calcule(operation);
+                        }
+                        else
+                        {
+                            resultat = Convert.ToDouble(l.Content);
+                        }
+                        operation = "X";
+                        break;
+                    case "/":
+                        if (operation != null)
+                        {
+                            Calcule(operation);
+                        }
+                        else
+                        {
+                            resultat = Convert.ToDouble(l.Content);
+                        }
+                        operation = "/";
+                        break;
+
+                }
+            }
+               
+        }
+
+        private void Calcule(string operation)
+        {
+            switch(operation)
+            {
+                case "+":
+                    resultat = resultat + Convert.ToDouble(l.Content);
+                    l.Content = resultat.ToString(); 
+                    break;
+                case "-":
+                    resultat = resultat - Convert.ToDouble(l.Content);
+                    l.Content = resultat.ToString();
+                    break;
+                case "X":
+                    resultat = resultat * Convert.ToDouble(l.Content);
+                    l.Content = resultat.ToString();
+                    break;
+                case "/":
+                    resultat = resultat / Convert.ToDouble(l.Content);
+                    l.Content = resultat.ToString();
+                    break;
             }
         }
 
