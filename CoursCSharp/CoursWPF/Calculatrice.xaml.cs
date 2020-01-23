8b82117211eab44ffb46f252bef2ae1f4902c71d
ihab <ihab@utopios.net>
2020-01-23 14:00:39 +0100

    Correction calculatrice partie 2

--- CoursCSharp/CoursWPF/Calculatrice.xaml.cs
+++ CoursCSharp/CoursWPF/Calculatrice.xaml.cs
@@ -19,9 +19,45 @@ namespace CoursWPF
     /// </summary>
     public partial class Calculatrice : Window
     {
+        private string[] tab = new string[] { "CE", "C", "supprimer", "%", "7", "8", "9", "X", "4", "5", "6", "-", "1", "2", "3", "+", "(+/-)", "0", ",", "=" };
         public Calculatrice()
         {
             InitializeComponent();
+            CreateRowsAndCols();
+            CreateElements();
+
         }
+
+        private void CreateRowsAndCols()
+        {
+            for(int i=0; i < 6; i++)
+            {
+                maGrille.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                if (i < 4)
+                    maGrille.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });              
+            }
+        }
+
+        private void CreateElements()
+        {
+            Label l = new Label() { Content = "0", VerticalContentAlignment = VerticalAlignment.Center, HorizontalContentAlignment = HorizontalAlignment.Right };
+            maGrille.Children.Add(l);
+            Grid.SetColumn(l, 0);
+            Grid.SetRow(l, 0);
+            Grid.SetColumnSpan(l, 4);
+            int index = 0;
+            for(int i= 1; i < 6; i++)
+            {
+                for(int j=0; j < 4; j++)
+                {
+                    Button b = new Button { Content = tab[index] };
+                    index++;
+                    maGrille.Children.Add(b);
+                    Grid.SetColumn(b, j);
+                    Grid.SetRow(b, i);
+                }
+            }
+        }
+
     }
 }
