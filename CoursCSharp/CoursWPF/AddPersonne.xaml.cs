1a9c63eefc88ce007d00ebf9fac3f6dea67ea049
ihab <ihab@utopios.net>
2020-01-24 09:42:52 +0100

    cours wpf partie 3

---
+++ CoursCSharp/CoursWPF/AddPersonne.xaml.cs
@@ -0,0 +1,37 @@
+﻿using CoursWPF.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoursWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour AddPersonne.xaml
+    /// </summary>
+    public partial class AddPersonne : Window
+    {
+        public AddPersonne()
+        {
+            InitializeComponent();
+        }
+
+        private void Valider_Click(object sender, RoutedEventArgs e)
+        {
+            Personne p = new Personne() { Nom = firstName.Text, Prenom = lastName.Text };
+
+            ListePersonnes window = new ListePersonnes(p);
+            window.Show();
+            //MessageBox.Show(firstName.Text + " " + lastName.Text);
+        }
+    }
+}
---
+++ CoursCSharp/CoursWPF/ListePersonnes.xaml.cs
@@ -0,0 +1,34 @@
+﻿using CoursWPF.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoursWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour ListePersonnes.xaml
+    /// </summary>
+    public partial class ListePersonnes : Window
+    {
+        public ListePersonnes()
+        {
+            InitializeComponent();
+        }
+
+        public ListePersonnes(Personne  personne) : this()
+        {
+            nom.Content = personne.Nom;
+            prenom.Content = personne.Prenom;
+        }
+    }
+}
