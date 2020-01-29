1d2b98e39161b5d1e82b1016127f69395c8cdb7f
ihab <ihab@utopios.net>
2020-01-29 09:40:51 +0100

    cours wpf context

---
+++ CoursCSharp/CorrectionBanque/SQLQuery2.sql
@@ -0,0 +1,1 @@
+﻿SELECT c.id as clientId, c.nom, c.prenom, c.telephone, cc.id as compteId, cc.solde, o.id, o.montant, o.dateOperation FROM client as c left join compte as cc on cc.clientId = c.Id left join operation as o on cc.Id = o.compteId where c.Id = 3011
