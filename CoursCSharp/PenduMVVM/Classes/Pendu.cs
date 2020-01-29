082905cf64288ecf4ec14280a2fd7f20d401b183
ihab <ihab@utopios.net>
2020-01-29 12:18:54 +0100

    correction pendu MVVM

---
+++ CoursCSharp/PenduMVVM/Classes/Pendu.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PenduMVVM.Classes
+{
+    public class Pendu
+    {
+        private static List<string> mots = new List<string>() { "google", "maison", "ordinateur", "newyork" };
+
+        private string mot;
+
+        private string masque;
+
+        private string motEnCours;
+        private int compteur;
+        public string Mot { get => mot; }
+        public string Masque { get => masque; }
+        public string MotEnCours { get => motEnCours; set => motEnCours = value; }
+        public int Compteur { get => compteur; }
+        public Pendu()
+        {
+            Random r = new Random();
+            int nbre = r.Next(0, mots.Count);
+            mot = mots[nbre];
+            masque = "";
+            for(int i=0; i<mot.Length; i++)
+            {
+                masque += "*";
+            }
+            compteur = 10;
+        }
+
+        public bool TesterLettre(string lettre)
+        {
+            string masqueTmp = "";
+            bool result = false;
+            for(int i=0; i < mot.Length; i++)
+            {
+                if(mot[i].ToString() == lettre)
+                {
+                    masqueTmp += lettre;
+                    result = true;
+                }
+                else
+                {
+                    masqueTmp += masque[i];
+                }
+            }
+            masque = masqueTmp;
+            if (!result)
+            {
+                compteur--;
+            }
+            return result;
+        }
+    }
+}
---
+++ CoursCSharp/PenduMVVM/ViewModels/PenduViewModel.cs
@@ -0,0 +1,44 @@
+﻿using GalaSoft.MvvmLight;
+using PenduMVVM.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PenduMVVM.ViewModels
+{
+    public class PenduViewModel: ViewModelBase
+    {
+        private Pendu pendu;
+        public string Masque
+        {
+            get => pendu.Masque;
+        }
+
+        private string lettre;
+
+        public string Lettre
+        {
+            get => lettre;
+            set  {
+                if(pendu.TesterLettre(value))
+                {
+                    RaisePropertyChanged("Masque");
+                }
+                else
+                {
+                    RaisePropertyChanged("Compteur");
+                }
+                lettre = "";
+            }
+        }
+
+        public int Compteur { get => pendu.Compteur; }
+
+        public PenduViewModel()
+        {
+            pendu = new Pendu();
+        }
+    }
+}
