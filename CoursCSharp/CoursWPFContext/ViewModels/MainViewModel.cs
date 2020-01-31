f8617dced6501e8c78021de3098729d722fe5a35
ihab <ihab@utopios.net>
2020-01-31 13:55:58 +0100

    cours command

--- CoursCSharp/CoursWPFContext/ViewModels/MainViewModel.cs
+++ CoursCSharp/CoursWPFContext/ViewModels/MainViewModel.cs
@@ -1,13 +1,16 @@
 ﻿using CoursWPFContext.Classes;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CoursWPFContext.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
         private Personne personne;
         private Adresse adresse;
@@ -19,11 +22,17 @@ namespace CoursWPFContext.ViewModels
         public string CodePostal { get => Adresse.CodePostal; set => Adresse.CodePostal = value; }
         public Personne Personne { get => personne; set => personne = value; }
         public Adresse Adresse { get => adresse; set => adresse = value; }
-
+        public ICommand SaveCommand { get; set; }
         public MainViewModel()
         {
             Personne = new Personne();
             Adresse = new Adresse();
+            SaveCommand = new RelayCommand(Save);
+        }
+
+        public void Save()
+        {
+            //Save personne
         }
     }
 }
