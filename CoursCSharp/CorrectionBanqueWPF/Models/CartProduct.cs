3cb359b0f26700b0c583cf81ca4c8919f2319618
ihab <ihab@utopios.net>
2020-01-31 13:17:28 +0100

    correction wpf panier

--- CoursCSharp/CorrectionBanqueWPF/Models/CartProduct.cs
+++ CoursCSharp/CorrectionBanqueWPF/Models/CartProduct.cs
@@ -1,23 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CorrectionBanqueWPF.Models
 {
-    public class CartProduct
+    public class CartProduct : INotifyPropertyChanged
     {
         private Product product;
 
         private int qty;
 
-        public int Qty { get => qty; set => qty = value; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Qty { get => qty; set
+            {
+                qty = value;
+                NotifyProperty("Qty");
+            }
+        }
         public Product Product { get => product; set => product = value; }
 
         public CartProduct()
         {
             Product = new Product();
+        }
+
+        private void NotifyProperty(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
