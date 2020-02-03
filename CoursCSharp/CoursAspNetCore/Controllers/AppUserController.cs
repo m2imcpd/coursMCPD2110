d58dd4bbb6e03221c423b83b3ebe475eff33b3f2
ihab <ihab@utopios.net>
2020-02-03 12:02:42 +0100

    cours asp.net core viewData

---
+++ CoursCSharp/CoursAspNetCore/Controllers/AppUserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoursAspNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoursAspNetCore.Controllers
+{
+    public class AppUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("ListUsers");
+        }
+
+        public IActionResult ListUsers()
+        {
+            List<AppUserModel> liste = AppUserModel.GetAllUsers();
+            //Passage en utilisant le ViewData
+            ViewData["listeUsers"] = liste;
+            return View();
+        }
+    }
+}
---
+++ CoursCSharp/CoursAspNetCore/Models/AppUserModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoursAspNetCore.Models
+{
+    public class AppUserModel
+    {
+        private int id;
+        private string nom;
+        private string prenom;
+
+        public int Id { get => id; set => id = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+
+        public static List<AppUserModel> GetAllUsers()
+        {
+            List<AppUserModel> liste = new List<AppUserModel>();
+            liste.Add(new AppUserModel { Nom = "toto", Prenom = "tata" });
+            liste.Add(new AppUserModel { Nom = "titi", Prenom = "minet" });
+
+            return liste;
+        }
+    }
+}
