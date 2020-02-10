2b78cecb183723038269083d840980c519251b24
ihab <ihab@utopios.net>
2020-02-10 13:50:45 +0100

    correction api annuaire partie 2

--- CoursCSharp/CoursAspNetApi/Controllers/ContactController.cs
+++ CoursCSharp/CoursAspNetApi/Controllers/ContactController.cs
@@ -54,5 +54,55 @@ namespace CoursAspNetApi.Controllers
                 return StatusCode(500);
             }
         }
+
+        [HttpPut("{phone}")]
+        public IActionResult Put(string phone, [FromBody] Contact contact)
+        {
+            Contact oldContact = Contact.GetContactByTelephone(phone);
+            if (oldContact != null)
+            {
+                oldContact.Emails = Email.GetEmailContact(oldContact.Id);
+                oldContact.Nom = contact.Nom != null ? contact.Nom : oldContact.Nom;
+                oldContact.Prenom = contact.Prenom != null ? contact.Prenom : oldContact.Prenom;
+                oldContact.Telephone = contact.Telephone != null ? contact.Telephone : oldContact.Telephone;
+                oldContact.Update();
+                if(contact.Emails.Count > 0)
+                {
+                    foreach(Email e in oldContact.Emails)
+                    {
+                        e.Delete();
+                    }
+                    foreach(Email e in contact.Emails)
+                    {
+                        e.Save(oldContact.Id);
+                    }
+                }
+                return Ok(new { message = "updated" });
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{phone}")]
+        public IActionResult Delete(string phone)
+        {
+            Contact c = Contact.GetContactByTelephone(phone);
+            if (c != null)
+            {
+                c.Emails = Email.GetEmailContact(c.Id);
+                foreach(Email e in c.Emails)
+                {
+                    e.Delete();
+                }
+                c.Delete();
+                return Ok(new { message = "Deleted" });
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
     }
 }
